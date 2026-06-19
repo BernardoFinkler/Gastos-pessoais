a3ab20e6a1ae6aa50660a5dd1ef2bb7f0898da4c
BernardoFinkler <bernardo.finkler1@cwi.com.br>
2026-06-19 11:48:25 -0300

    feat: adiciona api spring ao projeto

---
+++ api/src/test/java/com/gastospessoais/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gastospessoais.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
